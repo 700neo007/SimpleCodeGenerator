70382f222fecbb8517794caf141569bfffc6700e
Neo <huanongweiky@21cn.com>
2013-09-17 21:59:56 +0800

    去掉base\src\main\java\maven\code\generator\AbstractDao.java @Override

--- base/src/main/java/maven/code/generator/AbstractDao.java
+++ base/src/main/java/maven/code/generator/AbstractDao.java
@@ -11,17 +11,14 @@ public class AbstractDao<T> implements Dao<T> {
         this.type = type;
     }
 
-    @Override
     public T load(long id) {
         return (T) em.find(type, id);
     }
 
-    @Override
     public void save(T object) {
         em.persist(object);
     }
 
-    @Override
     public void update(T object) {
         em.refresh(object);
     }
